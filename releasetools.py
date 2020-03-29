f2f5e8280b5d901efe918c1d1a498106dc99be1f
Akhil Narang <me@akhilnarang.dev>
2020-03-29 18:48:25 +0530

    even: releasetools: flash vbmeta_system as well * add vbmeta_system support as well
    Change-Id: I2ecbf573fc7fa14ed7abff615c5353934f56aaee

--- releasetools.py
+++ releasetools.py
@@ -40,4 +40,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
   return
