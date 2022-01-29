971904761614888f7650bc8e47ef1692e7b2fe97
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-01-29 11:49:17 +0530

    even: Disable compressed APEX
    Compressed APEX has been introduced in Android 12, and they are enabled
    by default inheriting updatable_apex.mk.
    Unfortunately, when the device is encrypted, they are causing bootloops.
    
    As the only advantage is some space gain on disk, we can
    simply disable them: keep the variable immediatly after including the
    updatable_apex.mk to avoid being overwritten.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I4dd4000774cea69ac5ec364e4a69edffc587d5a0

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
+
 # IMS
 $(call inherit-product, vendor/realme/even-ims/even-ims.mk)
 
